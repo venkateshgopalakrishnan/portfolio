7053aff38c8915c61936eedb0319aa195fcfde8c
Venkatesh Gopalakrishnan <96gvenkatesh@gmail.com>
2019-03-06 01:07:19 +0530

    Updates

---
+++ precache-manifest.03b8555aa32460d92f2fa9bb6199e332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaeff95551887c6d493e",
+    "url": "/portfolio-app/static/js/runtime~main.eaeff955.js"
+  },
+  {
+    "revision": "f62e3e26f5395292930f",
+    "url": "/portfolio-app/static/js/main.f62e3e26.chunk.js"
+  },
+  {
+    "revision": "afd4758aeb1a6676a946",
+    "url": "/portfolio-app/static/js/2.afd4758a.chunk.js"
+  },
+  {
+    "revision": "f62e3e26f5395292930f",
+    "url": "/portfolio-app/static/css/main.a6eea244.chunk.css"
+  },
+  {
+    "revision": "afd4758aeb1a6676a946",
+    "url": "/portfolio-app/static/css/2.de6ee8cd.chunk.css"
+  },
+  {
+    "revision": "f3310574bb8e085e2e69231ae48ec81c",
+    "url": "/portfolio-app/index.html"
+  }
+];
